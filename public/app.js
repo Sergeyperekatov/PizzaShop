d26d4b2e4c7922d3f556740c661a8373c14cc7f2
Sergey Perekatov <perekatovsergey@yandex.ru>
2019-03-27 22:59:02 +0200

    Add numbers orders

--- public/app.js
+++ public/app.js
@@ -1,11 +1,26 @@
+function add(id)
+{
+	var key = 'product_' + id;
 
-function something()
-{	
-	var x = window.localStorage.getItem('aaa');
+	var x = window.localStorage.getItem(key);
+	x=x*1+1;
+	window.localStorage.setItem(key, x);
+}
 
-	x= x * 1 + 1;
-	window.localStorage.setItem('aaa', x);
-	
-	alert (x);
+function get_number_of_items
+{
+	for(var i = 0; i<window.localStorage.length; i++ )
 
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+
+		if(key.indexOf('product_')==0)
+		{
+			cnt=cnt+value;
+
+		}
+
+	}
+	return cnt;
 }
