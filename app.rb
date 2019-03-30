ba0e9613c10c626fabc85e29cf13acea8bf2a3a6
Sergey Perekatov <perekatovsergey@yandex.ru>
2019-03-30 22:46:49 +0200

    output forms for the data delivery

--- app.rb
+++ app.rb
@@ -16,15 +16,10 @@ get '/' do
 		erb :index		
 end
 
-get '/menu' do
-	
-	
-	erb :menu			
-end
 
 post '/cart' do
-	orders_input = params[:orders]	
-	@items = parse_orders_input orders_input
+	@orders_input = params[:orders]	
+	@items = parse_orders_input @orders_input
 	
 	@items.each do |item|
 		item[0] = Product.find(item[0])
@@ -37,7 +32,7 @@ end
 
 def parse_orders_input orders_input
 
-s1 = orders_input.split(/,/)
+s1 = @orders_input.split(/,/)
         arr =[]
 
 	s1.each do |x|
